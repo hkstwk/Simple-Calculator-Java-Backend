0e8685340224a3844f9c6017203294fd0ddef841
hkstwk <hkolvoort+github@gmail.com>
2020-11-01 23:17:51 +0100

    SimpleCalculator Initial Commit

---
+++ src/main/java/nl/kolvoort/simplecalculator/dto/CalculationOutput.java
@@ -0,0 +1,17 @@
+package nl.kolvoort.simplecalculator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+public @Data
+class CalculationOutput {
+
+    private int leftOperand;
+    private int rightOperand;
+    private String operator;
+
+    // Result as string in order to also return exceptions in resultset.
+    // Given correct input normally double would be returned
+    private String result;
+}
